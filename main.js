8ccf68d415c21df7dd39db8e9df2c076f41f3588
Joe McNeil <69708014+samarqandiy@users.noreply.github.com>
2023-11-18 18:42:23 +0500

    Create main.js
    main

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// vriable = 'Dave'
+// console.log(vriable);
+// // ReferenceError
+
+// Object. .create()
+// // SyntaxError
+
+//const name = 'Dave'
+// name = "Joe";
+// // TypeError
+
+// const makeaErr = () => {
+//   try {
+//     const name = "Dave";
+//     name = "Joe";
+//     console.table(name);
+//   } catch (err) {
+//     console.table(err);
+//   }
+// };
+// makeaErr()
+// // handling err
